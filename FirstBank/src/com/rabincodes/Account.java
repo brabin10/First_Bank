609dcfc01281b79c5f0d2e48a8c1595cc3652232
Rabin Basnet <rowbinr@yahoo.com>
2022-06-29 13:32:27 -0600

    project completed

--- FirstBank/src/com/rabincodes/Account.java
+++ FirstBank/src/com/rabincodes/Account.java
@@ -67,6 +67,66 @@ public class Account {
 		System.out.println("E. Calculate interest");
 		System.out.println("F. Exit");
 		
+		do {
+			System.out.println();
+			System.out.println("Enter an option:");
+			char option1 = scanner.next().charAt(0);
+			option = Character.toUpperCase(option1);
+			System.out.println();
+			
+			switch(option) {
+			//Case 'A' allows the user to check their account balance
+			case 'A':
+				System.out.println("===================================");
+				System.out.println("Balance = $" + balance);
+				System.out.println("===================================");
+				System.out.println();
+				break;
+				
+				//Case 'B' allows the user to deposit money into their account using 
+			case 'B':
+				System.out.println("Enter an amount to deposit: ");
+				int amount = scanner.nextInt();
+				deposit(amount);
+				System.out.println();
+				break;
+				
+				//Case 'C' allows the user to withdraw money from their account using
+			case 'C':
+				System.out.println("Enter an amount to wihdraw: ");
+				int amount2 = scanner.nextInt();
+				withdraw(amount2);
+				System.out.println();
+				break;
+				
+				//Case 'D' allows the user to view their most recent transaction using
+			case 'D':
+				System.out.println("==================================");
+				getPreviousTransaction();
+				System.out.println("==================================");
+				System.out.println();
+				break;
+				
+				//Case 'E' calculates the accrued interest on the account after a number
+			case 'E':
+				System.out.println("Enter how many years of accrued interest: ");
+				int years = scanner.nextInt();
+				calculateInterest(years);
+				break;
+				
+				//Case 'F' exits the user from their account
+			case 'F':
+				System.out.println("==================================");
+				break;
+				
+				//The default case let's the user know that they entered an invalid character
+				default:
+					System.out.println("Error: invalid option. Please enter A, B, C, D, E, F");
+					break;
+			}
+			
+		}while (option != 'F');
+		System.out.println("Thank you for banking with us!");
 	}
 	
 	
