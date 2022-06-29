33c060844091f3658706cbd9f0021818927d6364
Rabin Basnet <rowbinr@yahoo.com>
2022-06-29 12:41:57 -0600

    working on menu

---
+++ FirstBank/src/com/rabincodes/Account.java
@@ -0,0 +1,63 @@
+package com.rabincodes;
+
+import java.util.Scanner;
+
+public class Account {
+
+	int balance;
+	int previousTransaction;
+	String customerName;
+	String customerID;
+	
+	//Class constructor
+	Account(String cname, String cid) {
+		customerName = cname;
+		customerID = cid;
+	}
+	
+	//Function for Depositing money
+	void deposit(int amount) {
+		if(amount != 0) {
+			balance = balance + amount;
+			previousTransaction = amount;
+			
+		}
+	}
+	
+	//Function for Withdrawing money
+	void withdraw(int amount) {
+		if(amount != 0) {
+			balance = balance - amount;
+			previousTransaction = amount;
+		}
+	}
+	
+	//Function showing the previous transaction
+	void getPreviousTransaction() {
+		if (previousTransaction > 0) {
+			System.out.println("Deposited: " + previousTransaction);
+		} else if (previousTransaction < 0) {
+			System.out.println("Withdrawn: " + Math.abs(previousTransaction));
+		} else {
+			System.out.println("No transaction occured");
+		}
+	}
+	
+	//Function calculating interest of current funds after a number of years
+	void calculateInterest(int years) {
+		double interestRate = .0185;
+		double newBalance = (balance * interestRate * years) + balance;
+		System.out.println("The current interest rate is " + (100 * interestRate) + "%");
+		System.out.println("After " + years + " years, your balance will be: " + newBalance);
+	}
+	
+	//Function showing the main menu
+	void showMenu() {
+		char option = '\0';
+		Scanner  scanner = new Scanner(System.in);
+		
+	}
+	
+	
+	
+}
---
+++ FirstBank/src/com/rabincodes/Launcher.java
@@ -0,0 +1,12 @@
+package com.rabincodes;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		
+		Account ac = new Account("Rabin Basnet", "A0011001");
+		ac.showMenu();
+
+	}
+
+}
